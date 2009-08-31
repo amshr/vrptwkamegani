1d26d5908eac15a65c07288cb5ed9570abd045a8
kamegani <kamegani@2797b942-70ae-11de-9ad3-cbed566d3045>
2009-08-31 21:26:44 +0000

    Moving...

--- trunk/selectElements.m
+++
@@ -1,7 +0,0 @@
-function elements=selectElements(list, i, n)
-%returns n subsequent elements from the list, starting from the one at position i.
-
-elements = [];
-for j=i:i-1+n
-    elements=[elements list(j)];
-end
