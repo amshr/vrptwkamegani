5174d735e68e43d1699c0b8549bbc89f998dfc2b
kamegani <kamegani@2797b942-70ae-11de-9ad3-cbed566d3045>
2010-01-31 03:03:15 +0000

    Some fixes and Solomon tests done.

--- trunk/AntColony/totalDistance.m
+++ trunk/AntColony/totalDistance.m
@@ -13,4 +13,4 @@ for i=1:m-1
 	distance=distance+disttab(route(i)+1,route(i+1)+1);
 end
 distance=distance+disttab(route(m)+1,1);
-distance = n*truck_value + distance;
+distance = 50*n*truck_value + distance;
