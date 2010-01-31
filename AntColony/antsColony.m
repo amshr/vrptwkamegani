0aae63a554e7430ae492f60d869f5f595b32c0c9
kamegani <kamegani@2797b942-70ae-11de-9ad3-cbed566d3045>
2010-01-31 03:03:15 +0000

    Some fixes and Solomon tests done.

--- AntColony/antsColony.m
+++ AntColony/antsColony.m
@@ -15,19 +15,28 @@ first_cost = totalDistance(best_feasible, B, truck_value);
 best_feasible_cost = first_cost;
 phero=phero_initialize(phero, best_feasible_cost, m);
 tauZero = 1/(best_feasible_cost*m);
+a=0
 [best_route, phero]=make_way(capacity, clients, A, B, phero, tauZero);
-for k=1:50
+k=1
+%for k=1:2000
+while 1
+    k=k+1
     for i=1:numberOfAnts
         ant=i;
         [current_route, phero]=make_way(capacity, clients, A, B, phero, tauZero);
-        current_route
-        current_cost=totalDistance(current_route, B, truck_value)
+        current_cost=totalDistance(current_route, B, truck_value);
         if current_cost<best_feasible_cost
             best_feasible=current_route;
             best_feasible_cost=current_cost;
+            a=0;
         end
     end
     phero=global_update(phero, best_route, B, lambda);
+    phero=global_update(phero, best_route, B, lambda);
+    a=a+1
+    if (a>500)
+        break
+    end
 end
 truck_value
 pfih_sol
