1e1ea00f332ff275e1081f30996225835518a66c
kamegani <kamegani@2797b942-70ae-11de-9ad3-cbed566d3045>
2010-03-07 03:58:42 +0000

    Simplified innerVns.m

--- trunk/VNS/innerVns.m
+++ trunk/VNS/innerVns.m
@@ -5,20 +5,15 @@ function [routes imp cost]=innerVns(routes, A, disttab, capacity)
     while k<=2
         if k==1
             [routes, improvement]=twoInterchange(routes, A, disttab, capacity);
-            if improvement==0
-                k=k+1;
-            else
-                imp=1;
-            end
         end
         if k==2
             [routes, improvement]=crossExchange(A, disttab, routes, capacity);
-            if improvement==1
-                k=1;
-                imp=1;
-            else
-                k=k+1;
-            end
+        end
+        if improvement==1
+            k=1;
+            imp=1;
+        else
+            k=k+1;
         end
     end
     newCost=totalCost(routes, disttab, 0);
